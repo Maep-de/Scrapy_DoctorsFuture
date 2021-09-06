8bdcf07d90f5e6510d606f47fe14a75e6d9667a8
sadikatanzila <57591240+sadikatanzila@users.noreply.github.com>
2021-09-06 10:45:57 +0200

    Add files via upload

---
+++ DoctorsFuture/DoctorsFuture/spiders/quotes_spider.py
@@ -0,0 +1,25 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://www.deutsches-krankenhaus-verzeichnis.de/app/suche/bundesland/sachsen',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
